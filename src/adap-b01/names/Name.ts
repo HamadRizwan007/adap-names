38ede67486c2ed49427b6400f6678035d2fbfbbc
Hamad Rizwan <hamad.rizwan@fau.de>
2025-10-23 00:57:39 +0200

    Implement Name class for B01 homework

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -6,9 +6,9 @@ export const ESCAPE_CHARACTER = '\\';
  * Special characters within the string may need masking, if they are to appear verbatim.
  * There are only two special characters, the delimiter character and the escape character.
  * The escape character can't be set, the delimiter character can.
- * 
+ *
  * Homogenous name examples
- * 
+ *
  * "oss.cs.fau.de" is a name with four name components and the delimiter character '.'.
  * "///" is a name with four empty components and the delimiter character '/'.
  * "Oh\.\.\." is a name with one component, if the delimiter character is '.'.
@@ -19,8 +19,12 @@ export class Name {
     private components: string[] = [];
 
     /** Expects that all Name components are properly masked */
+    // @methodtype initialization-method
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+        this.components = [...other];
+        if (delimiter !== undefined) {
+            this.delimiter = delimiter;
+        }
     }
 
     /**
@@ -28,45 +32,52 @@ export class Name {
      * Control characters are not escaped (creating a human-readable string)
      * Users can vary the delimiter character to be used
      */
+    // @methodtype conversion-method
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        return this.components.join(delimiter);
     }
 
-    /** 
+    /**
      * Returns a machine-readable representation of Name instance using default control characters
      * Machine-readable means that from a data string, a Name can be parsed back in
      * The control characters in the data string are the default characters
      */
+    // @methodtype conversion-method
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        return this.components.join(DEFAULT_DELIMITER);
     }
 
+    // @methodtype get-method
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        return this.components[i];
     }
 
     /** Expects that new Name component c is properly masked */
+    // @methodtype set-method
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components[i] = c;
     }
 
-     /** Returns number of components in Name instance */
-     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+    /** Returns number of components in Name instance */
+    // @methodtype get-method
+    public getNoComponents(): number {
+        return this.components.length;
     }
 
     /** Expects that new Name component c is properly masked */
+    // @methodtype command-method
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components.splice(i, 0, c);
     }
 
     /** Expects that new Name component c is properly masked */
+    // @methodtype command-method
     public append(c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components.push(c);
     }
 
+    // @methodtype command-method
     public remove(i: number): void {
-        throw new Error("needs implementation or deletion");
+        this.components.splice(i, 1);
     }
-
 }
